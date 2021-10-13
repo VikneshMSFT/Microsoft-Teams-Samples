a127b03e683fc5b48bee011fc207bab9896b702f
Robin Gupta <robgup@microsoft.com>
2021-10-13 16:47:07 +0530

    adding properties

--- samples/app-hello-world/csharp/Microsoft.Teams.Samples.HelloWorld.Web/Model/DependencyReminder.cs
+++ samples/app-hello-world/csharp/Microsoft.Teams.Samples.HelloWorld.Web/Model/DependencyReminder.cs
@@ -7,10 +7,23 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Model
 {
     public class DependencyReminder
     {
-        public string ReminderAliases { get; set; }
+        public int ReminderId { get; set; }
+        public List<string> ReminderAliases { get; set; }
 
         public string Message { get; set; }
 
         public DateTime DeadlineDateTime { get; set; }
+
+        public int Interval { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime LastTriggeredDateTime { get; set; }
+
+        public List<string> ObjectID { get; set; }
+
+        public string TeamId { get; set; }
+        public string ThreadId { get; set; }
+        public string ChannelId { get; set; }
     }
 }
